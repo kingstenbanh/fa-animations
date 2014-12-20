81a1ca76b7d96a0d6b1e8dd16f3d9151ee834181
Kingsten Banh <kingsten.banh@gmail.com>
2014-12-19 19:42:25 -0800

    refactor the animating elements using grid layout

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ var Engine = famous.core.Engine;
 var Modifier = famous.core.Modifier;
 var StateModifier = famous.modifiers.StateModifier;
 
+Engine.setOptions({ appMode: false });
+
 var mainContext = Engine.createContext();
 
 var appView = new AppView();
--- src/util.js
+++ src/util.js
@@ -1,21 +1,53 @@
-// helper methods
-var defaultOptions = function(easingCurve) {
-  return {
-    size: [100, 100],
-    content: easingCurve,
-    properties: {
-      borderRadius: '100px',
-      textAlign: 'center',
-      lineHeight: '100px',
-      backgroundColor: '#FA5C4F',
-      color: 'white'
-    }
-  };
-};
+var GridLayout = famous.views.GridLayout;
 
-var setTransition = function(easingCurve) {
-  return {
-    duration: 1000,
-    curve: Easing[easingCurve]
-  };
-};
+function createGrid(easingCurve, dimensions) {
+  var grid = new GridLayout({
+    dimensions: dimensions
+  });
+
+  var types = ['in', 'out', 'inOut'];
+
+  var views = [];
+
+  for (var i = 0; i < dimensions[0] * dimensions[1]; i++) {
+    var view = new View();
+
+    var sizeModifier = new StateModifier({
+      size: [100, 100]
+    });
+
+    var transitionModifier = new StateModifier();
+    transitionModifier.setTransform(
+      Transform.translate(100, 100, 0),
+      { duration: 1000, curve: Easing[types[i] + easingCurve] }
+    );
+
+    var surface = new Surface({
+      content: types[i] + easingCurve,
+      size: [undefined, undefined],
+      properties: {
+        backgroundColor: '#FA5C4F',
+        color: '#fff',
+        textAlign: 'center',
+        lineHeight: '100px',
+        borderRadius: '50px'
+      }
+    });
+
+    view.add(sizeModifier).add(transitionModifier).add(surface);
+
+    views.push(view);
+  }
+
+  grid.sequenceFrom(views);
+
+  return grid;
+}
+
+
+
+
+
+
+
+
